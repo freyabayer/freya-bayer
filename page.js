54b5f37631de6fe003c21fea641f7d1cbb49ef61
freyabayer <freyajlb@hotmail.com>
2026-05-20 15:04:24 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:'40px',fontFamily:'Arial',background:'#F7EBD8',minHeight:'100vh'}}>
+      <h1 style={{fontSize:'72px',color:'#ff1178'}}>FESTIUNI</h1>
+      <h2>Festival Universitário da Canção</h2>
+    </main>
+  )
+}
